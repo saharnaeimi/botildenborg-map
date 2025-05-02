789bc325812bb44413e3b32ac0b9e23a1166bf88
unknown <s.naeimi.d@gmail.com>
2025-05-02 16:52:25 +0200

    Use basename for GitHub Pages routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import EditPage from './EditPage';
 
+const basename = process.env.PUBLIC_URL;  
+// On GitHub Pages this will be "/botildenborg-map"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/edit/:id" element={<EditPage />} />
+      <Route path="edit/:id" element={<EditPage />} />
+      {/* optional catch‑all: */}
+      <Route path="*" element={<App />} />
     </Routes>
   </BrowserRouter>
 );
